f58efe249764b579fecebff589d6975520850d55
maxcramer <maximiliancramer@yahoo.com>
2021-01-20 17:55:50 +0000

    styled articles for mobile

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -4,9 +4,9 @@ import './Article.css';
 
 const Article = (props) => (
     <div className="Article">
-        Author: {props.author}
-        Title: {props.title}
-        Description: {props.content.slice(0, 150)}...
+        <h4>{props.title}</h4>
+        <h5>By {props.author}</h5>
+        <p>{props.content.slice(0, 150)}...</p>
     </div>
 );
 
