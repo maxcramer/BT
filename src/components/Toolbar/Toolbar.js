58a0532d17048aacd625b9f051aa7e531603f5cc
maxcramer <maximiliancramer@yahoo.com>
2021-01-20 14:35:22 +0000

    building basic jsx in toolbar, changing app from functional component to class based compoent

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,2 +1,28 @@
 import React from 'react';
 
+const Toolbar = (props) => (
+    <header>
+        <nav>
+            <div>
+            </div>
+            <div>
+                <a href='/'>The Logo</a>
+            </div>
+            <div>
+                <ul>
+                    <li>
+                        <a href="/">Link 1</a>
+                    </li>
+                    <li>
+                        <a href="/">Link 2</a>
+                    </li>
+                    <li>
+                        <a href="/">Link 3</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
+
+export default Toolbar;
